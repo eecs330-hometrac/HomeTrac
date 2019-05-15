6044cdea9c48826ba3257c465335697737fa0357
Shawn <seungchoi2019@u.northwestern.edu>
2019-05-14 21:48:11 -0500

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,90 @@
+var properties = [
+    {
+        name: "E2",
+        campus: "South",
+        bedroom: "1",
+        bathroom: "1",
+        price: "2000",
+        lease_length: "12"
+    },
+    {
+        name: "1717",
+        campus: "South",
+        bedroom: "1",
+        bathroom: "1",
+        price: "1700",
+        lease_length: "3"
+    },
+    {
+        name: "Park Evanston",
+        campus: "South",
+        bedroom: "1",
+        bathroom: "1",
+        price: "1800",
+        lease_length: "12"
+    },
+    {
+        name: "The Main",
+        campus: "South",
+        bedroom: "1",
+        bathroom: "1",
+        price: "1900",
+        lease_length: "3"
+    }
+    {
+        name: "Evanston Place Apartments",
+        campus: "South",
+        bedroom: "1",
+        bathroom: "1",
+        price: "1600",
+        lease_length: "6"
+    }
+    {
+        name: "Plaza",
+        campus: "North",
+        bedroom: "2",
+        bathroom: "1",
+        price: "2200",
+        lease_length: "12"
+    },
+    {
+        name: "AMLI",
+        campus: "South",
+        bedroom: "2",
+        bathroom: "2",
+        price: "2200",
+        lease_length: "6"
+    },
+    {
+        name: "The Reserve",
+        campus: "South",
+        bedroom: "3",
+        bathroom: "2",
+        price: "3000",
+        lease_length: "12"
+    },
+    {
+        name: "829 Foster",
+        campus: "North",
+        bedroom: "1",
+        bathroom: "1",
+        price: "1400",
+        lease_length: "3"
+    },
+    {
+        name: "Ten Twenty Grove",
+        campus: "South",
+        bedroom: "1",
+        bathroom: "1",
+        price: "1600",
+        lease_length: "6"
+    },
+    {
+        name: "1500 Chicago Ave",
+        campus: "South",
+        bedroom: "3",
+        bathroom: "3",
+        price: "3300",
+        lease_length: "12"
+    },
+]
