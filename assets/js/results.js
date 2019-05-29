9c488cb4725d28380ac6562af3eeda4f646ca99b
vbass0903 <32740596+vbass0903@users.noreply.github.com>
2019-05-28 23:16:17 -0500

    Added compareTo.html, Fixed summary.html, Added Dynamic Comparison

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,4 +1,16 @@
       var locations = [
+          {
+          name : "Evanston Place",
+          address : "1715 Chicago Ave, Evanston, IL 60201",
+          campus_loc : "South",
+          num_bed : 1,
+          num_bath : 1,
+          rent : 300,
+          lease_len : "12 Months",
+          latitude: 42.049078,
+          longitude: -87.677995,
+          img : "images/evanston-place-apartments-evanston-il-meet-up-with-friends-on-the-social-deck-.jpg"
+        },
         {
           name : "Evanston Place",
           address : "1715 Chicago Ave, Evanston, IL 60201",
@@ -10,6 +22,30 @@
           latitude: 42.049078,
           longitude: -87.677995,
           img : "images/evanston-place-apartments-evanston-il-meet-up-with-friends-on-the-social-deck-.jpg"
+        },
+        {
+          name : "Evanston Place",
+          address : "1715 Chicago Ave, Evanston, IL 60201",
+          campus_loc : "South",
+          num_bed : 3,
+          num_bath : 3,
+          rent : 900,
+          lease_len : "12 Months",
+          latitude: 42.049078,
+          longitude: -87.677995,
+          img : "images/evanston-place-apartments-evanston-il-meet-up-with-friends-on-the-social-deck-.jpg"
+        },
+        {
+          name : "E2 Apartments",
+          address : "1890 Maple Ave, Evanston, IL 60201",
+          campus_loc : "South",
+          num_bed : 1,
+          num_bath : 1,
+          rent : 400,
+          lease_len : "12 Months",
+          latitude : 42.053437, 
+          longitude: -87.685229,
+          img : "images/e2_home.JPG"
         },
         {
           name : "E2 Apartments",
@@ -24,16 +60,208 @@
           img : "images/e2_home.JPG"
         },
         {
+          name : "E2 Apartments",
+          address : "1890 Maple Ave, Evanston, IL 60201",
+          campus_loc : "South",
+          num_bed : 3,
+          num_bath : 3,
+          rent : 1000,
+          lease_len : "12 Months",
+          latitude : 42.053437, 
+          longitude: -87.685229,
+          img : "images/e2_home.JPG"
+        },
+        {
+          name : "E2 Apartments",
+          address : "1890 Maple Ave, Evanston, IL 60201",
+          campus_loc : "South",
+          num_bed : 4,
+          num_bath : 3,
+          rent : 1200,
+          lease_len : "12 Months",
+          latitude : 42.053437, 
+          longitude: -87.685229,
+          img : "images/e2_home.JPG"
+        },
+        {
+          name : "Carlson Apartments",
+          address : "636 Church St # 400, Evanston, IL 60201",
+          campus_loc : "South",
+          num_bed : 1,
+          num_bath : 1,
+          rent : 300,
+          lease_len : "3 Months",
+          latitude : 42.048161,
+          longitude : -87.680473,
+          img : "images/Carlson-Building-Evanston.jpg"
+        },
+        {
+          name : "Carlson Apartments",
+          address : "636 Church St # 400, Evanston, IL 60201",
+          campus_loc : "South",
+          num_bed : 2,
+          num_bath : 1,
+          rent : 500,
+          lease_len : "3 Months",
+          latitude : 42.048161,
+          longitude : -87.680473,
+          img : "images/Carlson-Building-Evanston.jpg"
+        },
+        {
+          name : "Carlson Apartments",
+          address : "636 Church St # 400, Evanston, IL 60201",
+          campus_loc : "South",
+          num_bed : 3,
+          num_bath : 2,
+          rent : 700,
+          lease_len : "3 Months",
+          latitude : 42.048161,
+          longitude : -87.680473,
+          img : "images/Carlson-Building-Evanston.jpg"
+        },
+        {
           name : "Carlson Apartments",
           address : "636 Church St # 400, Evanston, IL 60201",
           campus_loc : "South",
           num_bed : 4,
           num_bath : 2,
-          rent : 600,
-          lease_len : "9 Months",
+          rent : 900,
+          lease_len : "3 Months",
           latitude : 42.048161,
           longitude : -87.680473,
           img : "images/Carlson-Building-Evanston.jpg"
+        },
+        {
+          name : "The Link Evanston",
+          address : "811 Emerson St, Evanston, IL 60201",
+          campus_loc : "North",
+          num_bed : 1,
+          num_bath : 1,
+          rent : 400,
+          lease_len : "6 Months",
+          latitude : 42.052278,
+          longitude : -87.682902,
+          img : "images/the-link.jpg"
+        },
+        {
+          name : "The Link Evanston",
+          address : "811 Emerson St, Evanston, IL 60201",
+          campus_loc : "North",
+          num_bed : 2,
+          num_bath : 2,
+          rent : 600,
+          lease_len : "6 Months",
+          latitude : 42.052278,
+          longitude : -87.682902,
+          img : "images/the-link.jpg"
+        },
+        {
+          name : "The Link Evanston",
+          address : "811 Emerson St, Evanston, IL 60201",
+          campus_loc : "North",
+          num_bed : 3,
+          num_bath : 3,
+          rent : 900,
+          lease_len : "6 Months",
+          latitude : 42.052278,
+          longitude : -87.682902,
+          img : "images/the-link.jpg"
+        },
+        {
+          name : "The Link Evanston",
+          address : "811 Emerson St, Evanston, IL 60201",
+          campus_loc : "North",
+          num_bed : 4,
+          num_bath : 4,
+          rent : 1300,
+          lease_len : "6 Months",
+          latitude : 42.052278,
+          longitude : -87.682902,
+          img : "images/the-link.jpg"
+        },
+        {
+          name : "The Reserve",
+          address : "1930 Ridge Ave, Evanston, IL 60201",
+          campus_loc : "North",
+          num_bed : 1,
+          num_bath : 1,
+          rent : 350,
+          lease_len : "9 Months",
+          latitude : 42.053497,
+          longitude : -87.687702,
+          img : "images/the-reserve.jpg"
+        },
+        {
+          name : "The Reserve",
+          address : "1930 Ridge Ave, Evanston, IL 60201",
+          campus_loc : "North",
+          num_bed : 2,
+          num_bath : 1,
+          rent : 550,
+          lease_len : "9 Months",
+          latitude : 42.053497,
+          longitude : -87.687702,
+          img : "images/the-reserve.jpg"
+        },
+        {
+          name : "The Reserve",
+          address : "1930 Ridge Ave, Evanston, IL 60201",
+          campus_loc : "North",
+          num_bed : 3,
+          num_bath : 1,
+          rent : 800,
+          lease_len : "9 Months",
+          latitude : 42.053497,
+          longitude : -87.687702,
+          img : "images/the-reserve.jpg"
+        },
+        {
+          name : "2257 Ridge Ave",
+          address : "2257 Ridge Ave, Evanston, IL 60201",
+          campus_loc : "North",
+          num_bed : 1,
+          num_bath : 1,
+          rent : 270,
+          lease_len : "12 Months",
+          latitude : 42.059363,
+          longitude : -87.685298,
+          img : "images/2257-ridge.jpg"
+        },
+        {
+          name : "2257 Ridge Ave",
+          address : "2257 Ridge Ave, Evanston, IL 60201",
+          campus_loc : "North",
+          num_bed : 2,
+          num_bath : 2,
+          rent : 450,
+          lease_len : "12 Months",
+          latitude : 42.059363,
+          longitude : -87.685298,
+          img : "images/2257-ridge.jpg"
+        },
+        {
+          name : "2257 Ridge Ave",
+          address : "2257 Ridge Ave, Evanston, IL 60201",
+          campus_loc : "North",
+          num_bed : 3,
+          num_bath : 3,
+          rent : 600,
+          lease_len : "12 Months",
+          latitude : 42.059363,
+          longitude : -87.685298,
+          img : "images/2257-ridge.jpg"
+        },
+        {
+          name : "2257 Ridge Ave",
+          address : "2257 Ridge Ave, Evanston, IL 60201",
+          campus_loc : "North",
+          num_bed : 4,
+          num_bath : 4,
+          rent : 1100,
+          lease_len : "12 Months",
+          latitude : 42.059363,
+          longitude : -87.685298,
+          img : "images/2257-ridge.jpg"
         }
       ];
 
@@ -100,7 +328,7 @@ function summ_func() {
     if (summ_name == loc.name) {
       //Modify .name_header, #img-fluid, #figure-img
       document.getElementById("name_header").innerHTML = loc.name;
-      document.getElementById("address_header").innerHTML = loc.campus_loc;
+      document.getElementById("address_header").innerHTML = loc.address;
       document.getElementById("img_header").src = loc.img;
       document.getElementById("sum_desc").innerHTML =
        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
@@ -117,7 +345,7 @@ function summ_func() {
       document.getElementById("sum_rent").innerHTML = "<u>Rent:</u> " + loc.rent;
       document.getElementById("sum_lease").innerHTML = "<u>Lease Length:</u> " + loc.lease_len;
 
-      document.getElementById("tour_btn").href = "contact.html?text=" + encodeURI(loc.name);
+      document.getElementById("compare_btn").href = "compareTo.html?text=" + encodeURI(loc.name);
       document.getElementById("tour_btn2").href = "contact.html?text=" + encodeURI(loc.name);
     }
   }
@@ -132,56 +360,84 @@ $(function(){
   });
 });
 
+function compareTo() {
+  let comp_name = decodeURI(getUrlParam('text','Empty'));
+  let campus_loc = localStorage.getItem("campus_val");
+  let num_bed = localStorage.getItem("bed_val");
+  let num_bath = localStorage.getItem("bath_val");
+  let rent_val = localStorage.getItem("rent_val");
 
+          if (rent_val == "$0 - $500") {
+            rent_low = 0;
+            rent_high = 500;
+          }
+          else if (rent_val == "$500 - $750") {
+            rent_low = 500;
+            rent_high = 750;
+          }
+          else if (rent_val == "$750 - $1000") {
+            rent_low = 750;
+            rent_high = 1000;
+          }
+          else {
+            rent_low = 1000;
+            rent_high = 100000;
+          }
+          
+          lease_len = localStorage.getItem("lease_val");
+          console.log(rent_val);
 
+          let x = 0;
+          let y = 0;
+          let i = 0;
+          let length = locations.length;
+          
+          var loc_list = document.getElementById("compare_list");
+          if (loc_list) {
+          
+              for (loc of locations) { //Loop over all locations
+                  if ((loc.campus_loc == campus_loc) && (loc.num_bed == num_bed) && (loc.num_bath == num_bath) && (loc.rent >= rent_low)
+                   && (loc.rent <= rent_high) && (loc.lease_len == lease_len)  && (loc.name != comp_name)) { //Location Filtering
+                      console.log("Found");
 
-/*$(function(){
-  
-  $("#search_btn").click(function(){
-      populate(locations,
-         localStorage.getItem("campus_val"),
-         localStorage.getItem("bed_val"),
-         localStorage.getItem("bath_val"),
-         0,
-         1000,
-         localStorage.getItem("lease_val"));
-  });
-});
-
-function populate() {
-    campus_loc = localStorage.getItem("campus_val");
-    num_bed = localStorage.getItem("bed_val");
-    num_bath = localStorage.getItem("bath_val");
-    rent_low = 0;
-    rent_high = 1000;
-    lease_len = localStorage.getItem("lease_val");
-
-    let x = 0;
-    let y = 0;
-    let length = locations.length;
-    
-    var loc_list = document.getElementById("result_list");
-    if (loc_list) {
-    
-        for (loc of locations) { //Loop over all locations
-
-            if ((loc.campus_loc == campus_loc) && (loc.num_bed == num_bed) && (loc.num_bath == num_bath) && (loc.rent >= rent_low)
-             && (loc.rent <= rent_high) && (loc.lease_len == lease_len)) { //Location Filtering
-
-                //Create a new list element and append it
-                let loc_result = document.createElement('li');
-                loc_result.classList.add("list-group-item");
-                loc_result.innerHTML =
-                    `<span>${loc.name}<br>Rent: ${loc.rent}<br>Beds: ${loc.num_bed}</span>
-                    <img style="max-width: 60%; max-height: 20%" class="float-right" src="${loc.img}"/>`;
-                loc_list.appendChild(loc_result);
-
-            }
-        }
-    }
+                      //Create a new list element and append it
+                      let loc_result = document.createElement('li');
+                      loc_result.classList.add("list-group-item");
+                      loc_result.innerHTML =
+                          `<span>${loc.name}<br>Rent: ${loc.rent}<br>Beds: ${loc.num_bed}</span>
+                          <a href="compare.html?first=${comp_name}&next=${loc.name}"><img style="max-width: 50%; max-height: 40%" class="float-right" src="${loc.img}"/></a>`;
+                      loc_list.appendChild(loc_result);
+                    }
+              }
+          }
 }
 
-//populate(locations,"South",2,2,0,1000,12);
-//console.log(localStorage.getItem("campus_val"));*/
+function compare() {
+  let first = decodeURI(getUrlParam('first','Empty'));
+  let next = decodeURI(getUrlParam('next','Empty'));
+  console.log(first,next);
+
+  for (loc of locations) {
+    if (first == loc.name) {
+      document.getElementById("first_name").innerHTML = loc.name;
+      document.getElementById("first_address").innerHTML = loc.address;
+      document.getElementById("first_img").src = loc.img;
+    }
+    if (next == loc.name) {
+      document.getElementById("next_name").innerHTML = loc.name;
+      document.getElementById("next_address").innerHTML = loc.address;
+      document.getElementById("next_img").src = loc.img;
+    }
+  }
+}
+
+function contact() {
+  let contact_name = decodeURI(getUrlParam('text','Empty'));
+  for (loc of locations) {
+    if (contact_name == loc.name) {
+      document.getElementById("contact_header").innerHTML = "Request a tour of " + loc.name;
+    }
+  } 
+}
 
 
